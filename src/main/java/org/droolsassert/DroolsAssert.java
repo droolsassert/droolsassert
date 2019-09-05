74c208f059713cb6ad53c59fd6a085d3ec1e28ca
Mike <madamovych@gmail.com>
2019-09-06 00:36:58 +0300

    Optimized loading resources and kie base creation to be loaded only once which makes difference for huge resources. Removed any notion of concurrency as it is not working reliably for parallel runs.

--- src/main/java/org/droolsassert/DroolsAssert.java
+++ src/main/java/org/droolsassert/DroolsAssert.java
@@ -10,9 +10,11 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
 import static org.apache.commons.collections4.CollectionUtils.subtract;
+import static org.apache.commons.lang3.ObjectUtils.firstNonNull;
 import static org.apache.commons.lang3.StringUtils.join;
 import static org.apache.commons.lang3.builder.ToStringBuilder.reflectionToString;
 import static org.apache.commons.lang3.builder.ToStringStyle.SHORT_PREFIX_STYLE;
+import static org.apache.commons.lang3.math.NumberUtils.INTEGER_ZERO;
 import static org.drools.core.impl.KnowledgeBaseFactory.newKnowledgeSessionConfiguration;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -30,8 +32,9 @@ import java.util.IdentityHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.WeakHashMap;
 import java.util.concurrent.TimeUnit;
 
 import org.drools.core.common.DefaultAgenda;
@@ -39,6 +42,7 @@ import org.drools.core.time.SessionPseudoClock;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
+import org.kie.api.KieBase;
 import org.kie.api.command.Command;
 import org.kie.api.event.rule.BeforeMatchFiredEvent;
 import org.kie.api.event.rule.DefaultAgendaEventListener;
@@ -59,24 +63,19 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
  * @see <a href=https://github.com/droolsassert/droolsassert>Documentation on GitHub</a>
  */
 public class DroolsAssert implements TestRule {
-
-	protected static DroolsSession droolsSessionMeta;
+	private static PathMatchingResourcePatternResolver resourceResolver = new PathMatchingResourcePatternResolver();
+	protected static Map<DroolsSession, KieBase> kieBases = new WeakHashMap<>();
+	
+	protected DroolsSession droolsSessionMeta;
 	protected AssertRules assertRulesMeta;
-	private static List<Resource> resources;
+	
 	protected KieSession session;
 	protected DefaultAgenda agenda;
 	protected SessionPseudoClock clock;
-	protected Map<String, Integer> activations = new ConcurrentHashMap<>();
+	protected Map<String, Integer> activations = new HashMap<>();
 	protected Set<String> ignored = new HashSet<>();
 	protected Map<Object, Integer> facts = new IdentityHashMap<>();
-
-	public DroolsAssert() {
-	}
-
-	public DroolsAssert(KieSession session) {
-		init(session);
-	}
-
+	
 	protected void init(KieSession session) {
 		this.session = session;
 		agenda = (DefaultAgenda) session.getAgenda();
@@ -84,46 +83,47 @@ public class DroolsAssert implements TestRule {
 		session.addEventListener(new LoggingAgendaEventListener());
 		session.addEventListener(new LoggingWorkingMemoryEventListener());
 	}
-
+	
 	protected KieSession newSession(DroolsSession droolsSessionMeta) {
+		Map<String, String> properties = defaultSessionProperties();
+		properties.putAll(toMap(false, droolsSessionMeta.properties()));
+		KieSessionConfiguration config = newKnowledgeSessionConfiguration();
+		for (Map.Entry<String, String> property : properties.entrySet())
+			config.setProperty(property.getKey(), property.getValue());
+		
+		return kieBase(droolsSessionMeta).newKieSession(config, null);
+	}
+	
+	protected KieBase kieBase(DroolsSession droolsSessionMeta) {
+		if (kieBases.containsKey(droolsSessionMeta))
+			return kieBases.get(droolsSessionMeta);
+		
 		try {
 			KieHelper kieHelper = new KieHelper();
-			for (Resource resource : resources(droolsSessionMeta))
+			for (Resource resource : getResources(droolsSessionMeta))
 				kieHelper.addResource(newUrlResource(resource.getURL()));
-
-			Map<String, String> properties = defaultSessionProperties();
-			properties.putAll(toMap(false, droolsSessionMeta.properties()));
-			KieSessionConfiguration config = newKnowledgeSessionConfiguration();
-			for (Map.Entry<String, String> property : properties.entrySet())
-				config.setProperty(property.getKey(), property.getValue());
-
-			return kieHelper.build().newKieSession(config, null);
+			kieBases.put(droolsSessionMeta, kieHelper.build());
+			return kieBases.get(droolsSessionMeta);
 		} catch (IOException e) {
 			throw new IllegalStateException("Cannot create new session", e);
 		}
 	}
-
-	private List<Resource> resources(DroolsSession droolsSessionMeta) throws IOException {
-		if (resources != null && DroolsAssert.droolsSessionMeta == droolsSessionMeta)
-			return resources;
-
-		resources = new ArrayList<>();
-		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+	
+	private List<Resource> getResources(DroolsSession droolsSessionMeta) throws IOException {
+		List<Resource> resources = new ArrayList<>();
 		for (String resourceNameFilter : firstNonEmpty(droolsSessionMeta.value(), droolsSessionMeta.resources()))
-			resources.addAll(asList(resolver.getResources(resourceNameFilter)));
-		assertTrue("No rules found", resources.size() > 0);
-
+			resources.addAll(asList(resourceResolver.getResources(resourceNameFilter)));
+		assertTrue("No resources found", resources.size() > 0);
+		
 		if (droolsSessionMeta.logResources())
 			resources.forEach(resource -> out.println(resource));
-
-		DroolsAssert.droolsSessionMeta = droolsSessionMeta;
 		return resources;
 	}
-
+	
 	public KieSession getSession() {
 		return session;
 	}
-
+	
 	@SuppressWarnings("unchecked")
 	public <T> T getObject(Class<T> clazz) {
 		Collection<T> objects = getObjects(clazz);
@@ -131,18 +131,18 @@ public class DroolsAssert implements TestRule {
 		assertFalse("Non-unique object of type found " + clazz.getSimpleName(), objects.size() > 1);
 		return (T) objects.toArray()[0];
 	}
-
+	
 	@SuppressWarnings("unchecked")
 	public <T> Collection<T> getObjects(Class<T> clazz) {
 		return (Collection<T>) session.getObjects(obj -> clazz.isInstance(obj));
 	}
-
+	
 	public void advanceTime(long amount, TimeUnit unit) {
 		clock.advanceTime(amount, unit);
 		// https://issues.jboss.org/browse/DROOLS-2240
 		session.fireAllRules();
 	}
-
+	
 	/**
 	 * Asserts the only rules listed have been activated no more no less.
 	 */
@@ -152,7 +152,7 @@ public class DroolsAssert implements TestRule {
 			expectedMap.put(rule, null);
 		assertActivations(expectedMap);
 	}
-
+	
 	/**
 	 * Asserts the only rules listed have been activated no more no less.<br>
 	 * Accepts the number of activations to assert.
@@ -162,21 +162,21 @@ public class DroolsAssert implements TestRule {
 				.filter(this::isEligibleForAssertion).collect(toSet());
 		Collection<String> extra = subtract(activations.keySet(), expectedCount.keySet()).stream()
 				.filter(this::isEligibleForAssertion).collect(toSet());
-
+		
 		if (!missing.isEmpty() && !extra.isEmpty())
 			fail(format("expected: %s unexpected: %s", missing, extra));
 		else if (!missing.isEmpty())
 			fail(format("expected: %s", missing));
 		else if (!extra.isEmpty())
 			fail(format("unexpected: %s", extra));
-
-		for (Map.Entry<String, Integer> actual : activations.entrySet()) {
+		
+		for (Entry<String, Integer> actual : activations.entrySet()) {
 			Integer expected = expectedCount.get(actual.getKey());
 			if (expected != null && !expected.equals(actual.getValue()))
 				fail(format("'%s' should be activated %s time(s) but actually it was activated %s time(s)", actual.getKey(), expected, actual.getValue()));
 		}
 	}
-
+	
 	/**
 	 * Asserts the only rules listed will be activated no more no less.<br>
 	 * Waits for scheduled rules if any.
@@ -187,7 +187,7 @@ public class DroolsAssert implements TestRule {
 			expectedMap.put(rule, null);
 		awaitForActivations(expectedMap);
 	}
-
+	
 	/**
 	 * Asserts the only rules listed will be activated no more no less.<br>
 	 * Waits for scheduled rules if any.<br>
@@ -197,7 +197,7 @@ public class DroolsAssert implements TestRule {
 		awaitForScheduledActivations();
 		assertActivations(expectedCount);
 	}
-
+	
 	/**
 	 * Await for any scheduled activations.
 	 */
@@ -207,11 +207,11 @@ public class DroolsAssert implements TestRule {
 		while (agenda.getActivations().length != 0)
 			advanceTime(50, MILLISECONDS);
 	}
-
+	
 	public void assertNoScheduledActivations() {
 		assertTrue("There are some scheduled activations.", agenda.getActivations().length == 0);
 	}
-
+	
 	/**
 	 * Asserts object presence in drools knowledge base.
 	 */
@@ -222,7 +222,7 @@ public class DroolsAssert implements TestRule {
 		}
 		fail("Object was not found in the session " + factToString(object));
 	}
-
+	
 	/**
 	 * Asserts object was retracted from knowledge base.
 	 */
@@ -232,7 +232,7 @@ public class DroolsAssert implements TestRule {
 				fail("Object was not retracted from the session " + factToString(retracted));
 		}
 	}
-
+	
 	/**
 	 * Asserts all objects were retracted from knowledge base.
 	 */
@@ -240,44 +240,44 @@ public class DroolsAssert implements TestRule {
 		List<Object> facts = new LinkedList<>(session.getObjects());
 		assertTrue("Objects were not retracted from the session: " + join(facts, lineSeparator()), facts.isEmpty());
 	}
-
+	
 	/**
 	 * Asserts exact count of facts in knowledge base.
 	 */
 	public void assertFactsCount(long factsCount) {
 		assertEquals(factsCount, session.getFactCount());
 	}
-
+	
 	/**
 	 * Define rules to be ignored while any assertions.
 	 */
 	public void ignoreActivations(String... rulePatterns) {
 		ignored.addAll(asList(rulePatterns).stream().map(s -> s.replaceAll("\\*", ".*?").replaceAll("_", ".")).collect(toSet()));
 	}
-
+	
 	/**
 	 * Define global variables for drools session
 	 */
 	public void setGlobal(String identifier, Object value) {
 		session.setGlobal(identifier, value);
 	}
-
+	
 	public <T> T execute(Command<T> command) {
 		return session.execute(command);
 	}
-
+	
 	public List<FactHandle> insert(Object... objects) {
 		List<FactHandle> factHandles = new LinkedList<>();
 		for (Object object : objects)
 			factHandles.add(session.insert(object));
 		return factHandles;
 	}
-
+	
 	public int fireAllRules() {
 		out.println("--> fireAllRules");
 		return session.fireAllRules();
 	}
-
+	
 	public List<FactHandle> insertAndFire(Object... objects) {
 		List<FactHandle> result = new LinkedList<>();
 		for (Object object : objects) {
@@ -286,7 +286,7 @@ public class DroolsAssert implements TestRule {
 		}
 		return result;
 	}
-
+	
 	public void printFacts() {
 		List<Object> sortedFacts = new LinkedList<>(session.getObjects());
 		sort(sortedFacts, (o1, o2) -> facts.get(o1).compareTo(facts.get(o2)));
@@ -294,15 +294,16 @@ public class DroolsAssert implements TestRule {
 		for (Object fact : sortedFacts)
 			out.println(factToString(fact));
 	}
-
+	
 	@Override
 	public Statement apply(Statement base, Description description) {
+		droolsSessionMeta = description.getTestClass().getAnnotation(DroolsSession.class);
 		assertRulesMeta = description.getAnnotation(AssertRules.class);
 		if (assertRulesMeta == null)
 			return base;
-
-		init(newSession(description.getTestClass().getAnnotation(DroolsSession.class)));
-
+		
+		init(newSession(droolsSessionMeta));
+		
 		return new Statement() {
 			@Override
 			public void evaluate() throws Throwable {
@@ -310,7 +311,7 @@ public class DroolsAssert implements TestRule {
 			}
 		};
 	}
-
+	
 	protected void evaluate(Statement base) throws Throwable {
 		List<Throwable> errors = new ArrayList<>();
 		try {
@@ -331,34 +332,34 @@ public class DroolsAssert implements TestRule {
 		session.dispose();
 		assertEmpty(errors);
 	}
-
+	
 	protected Map<String, String> defaultSessionProperties() {
 		return toMap(false, "drools.eventProcessingMode", "stream", "drools.clockType", "pseudo");
 	}
-
+	
 	protected boolean isEligibleForAssertion(String rule) {
 		return !ignored.stream().filter(regex -> rule.matches(regex)).findFirst().isPresent();
 	}
-
+	
 	protected String factToString(Object fact) {
 		return reflectionToString(fact, SHORT_PREFIX_STYLE);
 	}
-
+	
 	protected String tupleToString(List<Object> tuple) {
 		return "" + tuple.stream().map(o -> o.getClass().getSimpleName()).collect(toList());
 	}
-
+	
 	@SuppressWarnings("unchecked")
 	private <T> Map<String, T> toMap(boolean convertToInt, String... params) {
 		if (params.length % 2 != 0)
 			throw new IllegalStateException();
-
+		
 		Map<String, T> map = new HashMap<>();
 		for (int i = 0; i < params.length; i = i + 2)
 			map.put(params[i], (T) (convertToInt ? parseInt(params[i + 1]) : params[i + 1]));
 		return map;
 	}
-
+	
 	private String[] firstNonEmpty(String[]... params) {
 		for (String[] param : params) {
 			if (param.length != 0)
@@ -366,20 +367,16 @@ public class DroolsAssert implements TestRule {
 		}
 		return new String[0];
 	}
-
+	
 	private class LoggingAgendaEventListener extends DefaultAgendaEventListener {
 		@Override
 		public void beforeMatchFired(BeforeMatchFiredEvent event) {
 			String ruleName = event.getMatch().getRule().getName();
+			activations.put(ruleName, firstNonNull(activations.get(ruleName), INTEGER_ZERO) + 1);
 			out.println(format("<-- '%s' has been activated by the tuple %s", ruleName, tupleToString(event.getMatch().getObjects())));
-
-			synchronized (activations) {
-				Integer ruleActivations = activations.get(ruleName);
-				activations.put(ruleName, ruleActivations == null ? 1 : ruleActivations + 1);
-			}
 		}
 	}
-
+	
 	private class LoggingWorkingMemoryEventListener extends DefaultRuleRuntimeEventListener {
 		@Override
 		public void objectInserted(ObjectInsertedEvent event) {
@@ -388,13 +385,13 @@ public class DroolsAssert implements TestRule {
 				facts.put(fact, facts.size());
 			out.println("--> inserted: " + (droolsSessionMeta.logFacts() ? factToString(fact) : fact.getClass().getSimpleName()));
 		}
-
+		
 		@Override
 		public void objectDeleted(ObjectDeletedEvent event) {
 			Object fact = event.getOldObject();
 			out.println("--> retracted: " + (droolsSessionMeta.logFacts() ? factToString(fact) : fact.getClass().getSimpleName()));
 		}
-
+		
 		@Override
 		public void objectUpdated(ObjectUpdatedEvent event) {
 			out.println("--> updated: " + (droolsSessionMeta.logFacts()
