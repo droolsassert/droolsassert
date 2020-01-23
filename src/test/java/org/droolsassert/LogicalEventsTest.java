cfdf4bf121824366c90035366bc19b40c9aee213
Mike <madamovych@gmail.com>
2020-01-24 01:17:09 +0200

    Added testCallsConnectAndDisconnectLogicWithoutNumbers

--- src/test/java/org/droolsassert/LogicalEventsTest.java
+++ src/test/java/org/droolsassert/LogicalEventsTest.java
@@ -108,6 +108,37 @@ public class LogicalEventsTest {
 		drools.printFacts();
 	}
 	
+	@Test
+	@AssertRules({
+			"input call",
+			"drop dial-up if callee is talking",
+			"drop the call if caller is talking more than permitted time",
+			"call in progress dropped",
+			"input call dropped"
+	})
+	public void testCallsConnectAndDisconnectLogicWithoutNumbers() {
+		Dialing caller1Dial = new Dialing("11111", "22222");
+		drools.insertAndFire(caller1Dial);
+		drools.assertRetracted(caller1Dial);
+		CallInProgress call = drools.getObject(CallInProgress.class);
+		assertEquals("11111", call.callerNumber);
+		
+		Dialing caller3Dial = new Dialing("33333", "22222");
+		drools.insertAndFire(caller3Dial);
+		drools.assertExists(call);
+		drools.assertExists(caller3Dial);
+		
+		drools.awaitForScheduledActivations("drop dial-up if callee is talking");
+		drools.assertExists(call);
+		drools.assertRetracted(caller3Dial);
+		
+		drools.awaitForScheduledActivations("drop the call if caller is talking more than permitted time");
+		drools.assertRetracted(call);
+		drools.assertRetracted(caller3Dial);
+		
+		drools.assertAllRetracted();
+	}
+	
 	public static class Dialing {
 		public String callerNumber;
 		public String calleeNumber;
