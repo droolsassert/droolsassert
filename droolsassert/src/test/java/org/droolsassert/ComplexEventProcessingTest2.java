75384817bbef523ce33dcd8da43a3a1d384ccf17
Mike <madamovych@gmail.com>
2020-11-07 23:34:44 +0200

    Extend from the rule to omit boilerplate snippets

---
+++ droolsassert/src/test/java/org/droolsassert/ComplexEventProcessingTest2.java
@@ -0,0 +1,160 @@
+package org.droolsassert;
+
+import static java.util.concurrent.TimeUnit.HOURS;
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.assertEquals;
+
+import org.droolsassert.ComplexEventProcessingTest.CallInProgress;
+import org.droolsassert.ComplexEventProcessingTest.Dialing;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+@DroolsSession("classpath:/org/droolsassert/complexEventProcessing.drl")
+public class ComplexEventProcessingTest2 extends DroolsAssert {
+	
+	@Rule
+	public DroolsAssert drools = this;
+	
+	@Before
+	public void before() {
+		setGlobal("stdout", System.out);
+	}
+	
+	@Test
+	public void testCallsConnectAndDisconnectLogic() {
+		Dialing caller1Dial = new Dialing("11111", "22222");
+		insertAndFire(caller1Dial);
+		assertRetracted(caller1Dial);
+		CallInProgress call = getObject(CallInProgress.class);
+		assertEquals("11111", call.callerNumber);
+		
+		advanceTime(5, MINUTES);
+		Dialing caller3Dial = new Dialing("33333", "22222");
+		insertAndFire(caller3Dial);
+		assertExist(caller3Dial);
+		
+		advanceTime(5, SECONDS);
+		assertExist(call, caller3Dial);
+		
+		advanceTime(5, SECONDS);
+		assertExist(call);
+		assertRetracted(caller3Dial);
+		
+		advanceTime(1, HOURS);
+		assertRetracted(call);
+		
+		assertAllRetracted();
+	}
+	
+	@Test
+	public void testCallsConnectAndDisconnectLogicStickToEvents() {
+		Dialing caller1Dial = new Dialing("11111", "22222");
+		insertAndFire(caller1Dial);
+		assertActivated("input call");
+		assertRetracted(caller1Dial);
+		CallInProgress call = getObject(CallInProgress.class);
+		assertEquals("11111", call.callerNumber);
+		
+		Dialing caller3Dial = new Dialing("33333", "22222");
+		insertAndFire(caller3Dial);
+		assertActivated();
+		assertExist(call, caller3Dial);
+		
+		awaitFor("drop dial-up if callee is talking");
+		assertActivated("drop dial-up if callee is talking", "input call dropped");
+		assertExist(call);
+		assertRetracted(caller3Dial);
+		
+		awaitFor("drop the call if caller is talking more than permitted time");
+		assertActivatedCount(
+				1, "drop the call if caller is talking more than permitted time",
+				1, "call in progress dropped");
+		assertRetracted(call);
+		
+		assertNoScheduledActivations();
+		assertAllRetracted();
+	}
+	
+	@Test
+	@TestRules(expected = "input call")
+	public void testAssertActivations() {
+		insertAndFire(new Dialing("11111", "22222"));
+	}
+	
+	@Test
+	@TestRules(expected = {
+			"input call",
+			"drop the call if caller is talking more than permitted time",
+			"call in progress dropped"
+	}, checkScheduled = true)
+	public void testAssertScheduledActivations() {
+		insertAndFire(new Dialing("11111", "22222"));
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void testAssertNoScheduledActivations() {
+		insertAndFire(new Dialing("11111", "22222"));
+		assertNoScheduledActivations();
+	}
+	
+	@Test
+	@TestRules(expected = {
+			"input call",
+			"drop the call if caller is talking more than permitted time",
+			"call in progress dropped" })
+	public void testAwaitForAnyScheduledActivations() {
+		insertAndFire(new Dialing("11111", "22222"));
+		awaitForAny();
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void testAwaitScheduledActivations() {
+		insertAndFire(new Dialing("11111", "22222"));
+		awaitFor("blah");
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void testAwaitForAnyScheduledActivationsFailed() {
+		awaitForAny();
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void testAssertActivatedFailed() {
+		insertAndFire(new Dialing("11111", "22222"));
+		insertAndFire(new Dialing("33333", "44444"));
+		assertActivatedCount(1, "input call");
+	}
+	
+	@Test
+	@TestRules(expected = {
+			"input call",
+			"drop the call if caller is talking more than permitted time",
+			"call in progress dropped"
+	})
+	public void testAwaitForScheduledActivations() {
+		Dialing caller1Dial = new Dialing("11111", "22222");
+		insertAndFire(caller1Dial);
+		assertRetracted(caller1Dial);
+		CallInProgress call = getObject(CallInProgress.class);
+		assertEquals("11111", call.callerNumber);
+		
+		awaitFor("drop the call if caller is talking more than permitted time");
+		assertNoScheduledActivations();
+		assertAllRetracted();
+		
+		Dialing caller3Dial = new Dialing("33333", "22222");
+		insertAndFire(caller3Dial);
+		assertActivatedCount(
+				2, "input call",
+				1, "drop the call if caller is talking more than permitted time",
+				1, "call in progress dropped");
+		assertRetracted(caller3Dial);
+		call = getObject(CallInProgress.class);
+		assertEquals("33333", call.callerNumber);
+		
+		assertFactsCount(1);
+		printFacts();
+	}
+}
---
+++ droolsassert/src/test/java/org/droolsassert/DroolsAssertTest2.java
@@ -0,0 +1,89 @@
+package org.droolsassert;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+@DroolsSession(resources = {
+		"classpath*:/org/droolsassert/rules.drl",
+		"classpath*:/com/company/project/*/{regex:.*.(drl|dsl|xlsx|gdst)}",
+		"classpath*:/com/company/project/*/ruleUnderTest.rdslr" },
+		ignoreRules = { "before", "after" },
+		keepFactsHistory = false,
+		logResources = true)
+public class DroolsAssertTest2 extends DroolsAssert {
+	
+	@Rule
+	public DroolsAssert drools = this;
+	
+	@Test
+	@TestRules(expected = "atomic int rule")
+	public void testInt() {
+		insertAndFire(new AtomicInteger());
+		assertEquals(1, getObject(AtomicInteger.class).get());
+	}
+	
+	@Test
+	@TestRules(expected = { "atomic int rule", "atomic long rule" })
+	public void testLong() {
+		insert(new AtomicInteger(), new AtomicLong(), new AtomicLong());
+		fireAllRules();
+		assertFactsCount(3);
+		assertEquals(2, getObjects(AtomicLong.class).size());
+	}
+	
+	@Test
+	@TestRules(expectedCount = { "2", "atomic long rule" }, ignore = "* int rule")
+	public void testActivationCount() {
+		insertAndFire(new AtomicInteger(), new AtomicLong(), new AtomicLong());
+		assertEquals(2, getObjects(AtomicLong.class).size());
+	}
+	
+	@Test
+	@TestRules(expectedSource = "org/droolsassert/expectedDroolsAssertTest.txt")
+	public void testExpectedSource() {
+		insert(new AtomicInteger(), new AtomicLong(), new AtomicLong());
+		fireAllRules();
+		assertFactsCount(3);
+		assertEquals(2, getObjects(AtomicLong.class).size());
+	}
+	
+	@Test
+	@TestRules(expectedCountSource = "**/expectedCountDroolsAssertTest.txt", ignoreSource = "**/ignoreDroolsAssertTest.txt")
+	public void testExpectedCountSource() {
+		insert(new AtomicInteger(), new AtomicLong(), new AtomicLong());
+		fireAllRules();
+		assertFactsCount(3);
+		assertEquals(2, getObjects(AtomicLong.class).size());
+	}
+	
+	@Test
+	@TestRules(expected = {})
+	public void testNoRulesWereTriggered() {
+		insertAndFire("string");
+		assertFactsCount(1);
+		assertEquals("string", getObject(String.class));
+	}
+	
+	@Test(expected = AssertionError.class)
+	public void testNoObjectFound() {
+		assertEquals(0, getObject(BigDecimal.class).intValue());
+	}
+	
+	@Test(expected = AssertionError.class)
+	@TestRules(expected = "atomic long rule")
+	public void testNoUniqueObjectFound() {
+		insertAndFire(new AtomicLong(), new AtomicLong());
+		assertEquals(2, getObject(AtomicLong.class));
+	}
+	
+	@Test
+	public void testPrintFactsSkippedWhenHistoryIsDisabled() {
+		printFacts();
+	}
+}
