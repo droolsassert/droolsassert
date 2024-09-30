df0e297af5152b683453ab5c14067b1d903e0e6d
Mike <madamovych@gmail.com>
2024-10-01 00:10:14 +0300

    Introduced @TestRules#givenTime and DroolsAssert.advanceTimeTo()
    # Conflicts:
    #	droolsassert/src/main/java/org/droolsassert/DroolsAssert.java

---
+++ droolsassert/src/test/java/org/droolsassert/issue/DateEffectiveTest.java
@@ -0,0 +1,63 @@
+package org.droolsassert.issue;
+
+import static org.junit.Assert.assertEquals;
+
+import org.droolsassert.DroolsAssert;
+import org.droolsassert.DroolsSession;
+import org.droolsassert.TestRules;
+import org.junit.Rule;
+import org.junit.Test;
+
+@DroolsSession("**/issue/DateEffective.drl")
+public class DateEffectiveTest {
+
+	public static class Policy {
+		public String policyNumber;
+		public String stateCode;
+	}
+
+	@Rule
+	public DroolsAssert drools = new DroolsAssert();
+
+	@Test
+	@TestRules(givenTime = "2024-09-15 00:00:01", expected = "Simple")
+	public void testGivenTime20240915() {
+		Policy policy = new Policy();
+		policy.policyNumber = "1234";
+
+		drools.insertAndFire(policy);
+
+		assertEquals("OH", policy.stateCode);
+	}
+
+	@Test
+	@TestRules(givenTime = "2024-09-14", expected = {})
+	public void testGivenTime20240914() {
+		Policy policy = new Policy();
+		policy.policyNumber = "1234";
+
+		drools.insertAndFire(policy);
+
+		assertEquals(null, policy.stateCode);
+	}
+
+	@Test
+	@TestRules(givenTime = "2024-09-14", expectedCount = { "1", "Simple" })
+	public void testAdvanceTimeTo() {
+		Policy policy1 = new Policy();
+		policy1.policyNumber = "1234";
+		Policy policy2 = new Policy();
+		policy2.policyNumber = "1234";
+
+		drools.insertAndFire(policy1);
+		drools.assertActivated();
+		drools.advanceTimeTo("2024-09-15 00:00:01");
+		drools.assertActivated();
+		drools.insertAndFire(policy2);
+		drools.assertActivated("Simple");
+		drools.assertFactsCount(2);
+
+		assertEquals(null, policy1.stateCode);
+		assertEquals("OH", policy2.stateCode);
+	}
+}
