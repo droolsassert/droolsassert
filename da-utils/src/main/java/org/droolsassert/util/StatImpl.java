c79f72516cabce193949da7dc43da9e9f764dcac
Mike <madamovych@gmail.com>
2026-03-25 00:36:36 +0200

    Removed floating point computation

--- da-utils/src/main/java/org/droolsassert/util/StatImpl.java
+++ da-utils/src/main/java/org/droolsassert/util/StatImpl.java
@@ -2,7 +2,9 @@ package org.droolsassert.util;
 
 import static java.lang.String.format;
 import static java.lang.System.currentTimeMillis;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.droolsassert.util.PerfStat.round;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -10,21 +12,21 @@ public final class StatImpl implements Stat {
 	private static final long serialVersionUID = 6025961415245995217L;
 	private String type;
 	private String name;
-	volatile long leapsCount;
-	volatile long leapsCountSample;
-	volatile long failedLeapsCount;
-	volatile double totalTimeNs;
-	volatile double totalTimeSampleNs;
-	volatile double avgTimeSampleMs;
-	volatile double leapTimeMs;
-	volatile double minTimeMs;
-	volatile double minTimeSampleMs;
-	volatile double minTimeThresholdMs;
-	volatile double maxTimeMs;
-	volatile double maxTimeSampleMs;
-	volatile double maxTimeThresholdMs;
+	long leapsCount;
+	long leapsCountSample;
+	long failedLeapsCount;
+	long totalTimeNs;
+	long totalTimeSampleNs;
+	long avgTimeSampleNs;
+	long leapTimeNs;
+	long minTimeNs;
+	long minTimeSampleNs;
+	long minTimeThresholdNs;
+	long maxTimeNs;
+	long maxTimeSampleNs;
+	long maxTimeThresholdNs;
 	final AtomicLong peersCount = new AtomicLong();
-	volatile long lastAggregationTimeMs = currentTimeMillis();
+	volatile long lastAggregationTimeNs = MILLISECONDS.toNanos(currentTimeMillis());
 	
 	public StatImpl() {
 		// for deserialization
@@ -42,14 +44,15 @@ public final class StatImpl implements Stat {
 		failedLeapsCount = 0;
 		totalTimeNs = 0;
 		totalTimeSampleNs = 0;
-		avgTimeSampleMs = 0;
-		leapTimeMs = 0;
-		minTimeMs = 0;
-		minTimeSampleMs = 0;
-		minTimeThresholdMs = 0;
-		maxTimeMs = 0;
-		maxTimeSampleMs = 0;
-		maxTimeThresholdMs = 0;
+		avgTimeSampleNs = 0;
+		leapTimeNs = 0;
+		minTimeNs = 0;
+		minTimeSampleNs = 0;
+		minTimeThresholdNs = 0;
+		maxTimeNs = 0;
+		maxTimeSampleNs = 0;
+		maxTimeThresholdNs = 0;
+		lastAggregationTimeNs = MILLISECONDS.toNanos(currentTimeMillis());
 	}
 	
 	@Override
@@ -64,56 +67,58 @@ public final class StatImpl implements Stat {
 	
 	@Override
 	public String getFullName() {
-		return type == EMPTY ? name : type + "/" + name;
+		return EMPTY.equals(type) ? name : type + "/" + name;
 	}
 	
 	@Override
-	public long getLeapsCount() {
+	public synchronized long getLeapsCount() {
 		return leapsCount;
 	}
 	
 	@Override
-	public double getLeapTimeMs() {
-		return leapTimeMs;
+	public synchronized double getLeapTimeMs() {
+		return round(leapTimeNs);
 	}
 	
 	@Override
-	public double getMinTimeMs() {
-		return minTimeMs;
+	public synchronized double getMinTimeMs() {
+		return round(minTimeNs);
 	}
 	
 	@Override
-	public double getMinTimeSampleMs() {
-		return minTimeSampleMs;
+	public synchronized double getMinTimeSampleMs() {
+		return round(minTimeSampleNs);
 	}
 	
 	@Override
-	public double getMaxTimeMs() {
-		return maxTimeMs;
+	public synchronized double getMaxTimeMs() {
+		return round(maxTimeNs);
 	}
 	
 	@Override
-	public double getMaxTimeSampleMs() {
-		return maxTimeSampleMs;
+	public synchronized double getMaxTimeSampleMs() {
+		return round(maxTimeSampleNs);
 	}
 	
 	@Override
-	public double getAvgTimeMs() {
-		return PerfStat.round(totalTimeNs / leapsCount);
+	public synchronized double getAvgTimeMs() {
+		if (leapsCount == 0)
+			return 0;
+		return round(totalTimeNs / leapsCount);
 	}
 	
 	@Override
-	public double getAvgTimeSampleMs() {
-		return avgTimeSampleMs;
+	public synchronized double getAvgTimeSampleMs() {
+		return round(avgTimeSampleNs);
 	}
 	
 	@Override
-	public double getTotalTimeMs() {
-		return PerfStat.round(totalTimeNs);
+	public synchronized double getTotalTimeMs() {
+		return round(totalTimeNs);
 	}
 	
 	@Override
-	public long getFailedLeapsCount() {
+	public synchronized long getFailedLeapsCount() {
 		return failedLeapsCount;
 	}
 	
@@ -124,6 +129,17 @@ public final class StatImpl implements Stat {
 	
 	@Override
 	public String toString() {
-		return format("%,.2f %,.2f %,.2f", minTimeMs, getAvgTimeMs(), maxTimeMs);
+	    long min;
+	    long total;
+	    long count;
+	    long max;
+	    synchronized (this) {
+	        min = minTimeNs;
+	        total = totalTimeNs;
+	        count = leapsCount;
+	        max = maxTimeNs;
+	    }
+	    double avg = count == 0 ? 0 : round(total / count);
+	    return format("%,.2f %,.2f %,.2f", round(min), avg, round(max));
 	}
 }
