13c17577da2566d4e7ed5220faa97697cf493579
Mike <madamovych@gmail.com>
2026-03-25 00:36:36 +0200

    Removed floating point computation

--- da-utils/src/test/java/org/droolsassert/util/PerfStatTest.java
+++ da-utils/src/test/java/org/droolsassert/util/PerfStatTest.java
@@ -11,16 +11,16 @@ public class PerfStatTest {
 	@Test
 	@Disabled("for manual run")
 	public void test() {
-		float cycles = 1_000_000_000;
-		PerfStat domainPerf = new PerfStat("domain.under.test"); // ~0,002ms
+		float cycles = 1_000_000;
+		PerfStat domainPerf = new PerfStat("domain.under.test"); // ~0.006185 ms
 		
 		long start = currentTimeMillis();
 		for (int i = 0; i < cycles; i++) {
+			//new PerfStat("" + i);
 			domainPerf.start();
 			domainPerf.stop();
 		}
 		
-		out.printf("cycle time %f", ((currentTimeMillis() - start) / cycles)); // ~0.000150ms
+		out.printf("cycle time %f", ((currentTimeMillis() - start) / cycles)); // ~0.000160 ms
 	}
-	
 }
